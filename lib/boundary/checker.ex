76949b6d4bf80a8cb465fdf8cdca19fb2ddaa1ff
Saša Jurić <sasa.juric@gmail.com>
2020-02-22 22:29:05 +0100

    Refactor checker

--- lib/boundary/checker.ex
+++ lib/boundary/checker.ex
@@ -3,38 +3,38 @@ defmodule Boundary.Checker do
 
   # credo:disable-for-this-file Credo.Check.Readability.Specs
 
-  def errors(app, calls) do
+  def errors(spec, calls) do
     Enum.concat([
-      invalid_deps(app.boundaries),
-      cycles(app.boundaries),
-      unclassified_modules(app.modules.unclassified),
-      invalid_calls(app.boundaries, app.modules.classified, calls)
+      invalid_deps(spec),
+      cycles(spec),
+      unclassified_modules(spec),
+      invalid_calls(spec, calls)
     ])
   end
 
-  defp invalid_deps(boundaries) do
-    boundaries
+  defp invalid_deps(spec) do
+    spec.boundaries
     |> Stream.flat_map(fn {_boundary, data} -> Enum.map(data.deps, &%{name: &1, file: data.file, line: data.line}) end)
-    |> Stream.map(&validate_dep(boundaries, &1))
+    |> Stream.map(&validate_dep(spec, &1))
     |> Stream.reject(&is_nil/1)
     |> Stream.uniq()
   end
 
-  defp validate_dep(boundaries, dep) do
+  defp validate_dep(spec, dep) do
     cond do
-      not Map.has_key?(boundaries, dep.name) -> {:unknown_dep, dep}
-      Map.fetch!(boundaries, dep.name).ignore? -> {:ignored_dep, dep}
+      not Map.has_key?(spec.boundaries, dep.name) -> {:unknown_dep, dep}
+      Map.fetch!(spec.boundaries, dep.name).ignore? -> {:ignored_dep, dep}
       true -> nil
     end
   end
 
-  defp cycles(boundaries) do
+  defp cycles(spec) do
     graph = :digraph.new([:cyclic])
 
     try do
-      Enum.each(Map.keys(boundaries), &:digraph.add_vertex(graph, &1))
+      Enum.each(Map.keys(spec.boundaries), &:digraph.add_vertex(graph, &1))
 
-      boundaries
+      spec.boundaries
       |> Stream.flat_map(fn {boundary, data} -> Stream.map(data.deps, &{boundary, &1}) end)
       |> Enum.each(fn {boundary, dep} -> :digraph.add_edge(graph, boundary, dep) end)
 
@@ -48,61 +48,45 @@ defmodule Boundary.Checker do
     end
   end
 
-  defp unclassified_modules(unclassified_modules) do
-    unclassified_modules
+  defp unclassified_modules(spec) do
+    spec.modules.unclassified
     |> Stream.reject(& &1.protocol_impl?)
     |> Stream.map(&{:unclassified_module, &1.name})
   end
 
-  defp invalid_calls(boundaries, classified_modules, calls) do
-    calls
-    |> Stream.filter(&Map.has_key?(classified_modules, &1.callee_module))
-    |> Stream.filter(&Map.has_key?(classified_modules, &1.caller_module))
-    |> Enum.sort_by(&{&1.file, &1.line})
-    |> Stream.map(&call_error(&1, boundaries, classified_modules))
-    |> Stream.reject(&is_nil/1)
-    |> Stream.map(&{:invalid_call, &1})
-  end
-
-  defp call_error(entry, boundaries, classified_modules) do
-    from_boundary = Map.fetch!(classified_modules, entry.caller_module)
-    to_boundary = Map.fetch!(classified_modules, entry.callee_module)
-
-    cond do
-      Map.fetch!(boundaries, from_boundary).ignore? or Map.fetch!(boundaries, to_boundary).ignore? ->
-        nil
-
-      not allowed?(boundaries, from_boundary, to_boundary) ->
-        %{
-          type: :invalid_cross_boundary_call,
-          from_boundary: from_boundary,
-          to_boundary: to_boundary,
-          callee: entry.callee,
-          caller: entry.caller_module,
-          file: entry.file,
-          line: entry.line
-        }
-
-      from_boundary != to_boundary and not exported?(boundaries, to_boundary, entry.callee_module) ->
-        %{
-          type: :not_exported,
-          from_boundary: from_boundary,
-          to_boundary: to_boundary,
-          caller: entry.caller_module,
-          callee: entry.callee,
-          file: entry.file,
-          line: entry.line
-        }
-
-      true ->
-        nil
+  defp invalid_calls(spec, calls) do
+    for call <- calls,
+        from_boundary = Map.get(spec.modules.classified, call.caller_module),
+        not is_nil(from_boundary) and not Map.fetch!(spec.boundaries, from_boundary).ignore?,
+        to_boundary = Map.get(spec.modules.classified, call.callee_module),
+        not is_nil(to_boundary) and not Map.fetch!(spec.boundaries, to_boundary).ignore?,
+        from_boundary != to_boundary,
+        error = call_error(spec, call, from_boundary, to_boundary),
+        not is_nil(error) do
+      {:invalid_call,
+       %{
+         type: error,
+         from_boundary: from_boundary,
+         to_boundary: to_boundary,
+         callee: call.callee,
+         caller: call.caller_module,
+         file: call.file,
+         line: call.line
+       }}
     end
   end
 
-  defp allowed?(boundaries, from_boundary, to_boundary) do
-    from_boundary == to_boundary or
-      Enum.any?(Map.fetch!(boundaries, from_boundary).deps, &(&1 == to_boundary))
+  defp call_error(spec, call, from_boundary, to_boundary) do
+    cond do
+      not allowed?(spec.boundaries, from_boundary, to_boundary) -> :invalid_cross_boundary_call
+      not exported?(spec.boundaries, to_boundary, call.callee_module) -> :not_exported
+      true -> nil
+    end
   end
 
-  defp exported?(boundaries, boundary, module), do: Enum.any?(Map.fetch!(boundaries, boundary).exports, &(&1 == module))
+  defp allowed?(boundaries, from_boundary, to_boundary),
+    do: Enum.any?(Map.fetch!(boundaries, from_boundary).deps, &(&1 == to_boundary))
+
+  defp exported?(boundaries, boundary, module),
+    do: Enum.any?(Map.fetch!(boundaries, boundary).exports, &(&1 == module))
 end
