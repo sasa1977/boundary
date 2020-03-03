caa6dc3d2dd0282a44e32052ae809bc7625b7a71
Saša Jurić <sasa.juric@gmail.com>
2020-03-03 11:55:25 +0100

    Eliminate duplicates
    If compiler is running in-memory, the xref server is started only once, which leads to duplicate reports on repeated recompilations.

--- lib/boundary/mix/xref.ex
+++ lib/boundary/mix/xref.ex
@@ -2,12 +2,18 @@ defmodule Boundary.Mix.Xref do
   @moduledoc false
   use GenServer
 
-  @vsn 1
   @calls_table __MODULE__.Calls
   @seen_table __MODULE__.Seen
 
   @spec start_link :: GenServer.on_start()
-  def start_link, do: GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  def start_link do
+    result = GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+
+    if match?({:ok, _pid}, result) or match?({:error, {:already_started, _pid}}, result),
+      do: :ets.delete_all_objects(@seen_table)
+
+    result
+  end
 
   @spec add_call(module, %{callee: mfa, file: String.t(), line: non_neg_integer, mode: :compile | :runtime}) :: :ok
   def add_call(caller, call) do
@@ -24,6 +30,7 @@ defmodule Boundary.Mix.Xref do
     |> Stream.reject(&MapSet.member?(app_modules, &1))
     |> Enum.each(&:ets.delete(@calls_table, &1))
 
+    :ets.delete_all_objects(@seen_table)
     :ets.tab2file(@calls_table, to_charlist(manifest()))
   end
 
