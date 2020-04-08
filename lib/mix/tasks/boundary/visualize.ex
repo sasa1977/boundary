bc5ce20a8aab97a260eecf7b81c2c5e0e55d023d
Pol Miro <polmiro@gmail.com>
2020-04-08 16:00:50 +0200

    Generate output for root boundary level

--- lib/mix/tasks/boundary/visualize.ex
+++ lib/mix/tasks/boundary/visualize.ex
@@ -9,37 +9,55 @@ defmodule Mix.Tasks.Boundary.Visualize do
 
   @impl Mix.Task
   def run(_argv) do
-    name = "Test.Graph"
-    edges = [{"A", "B"}, {"A", "C"}, {"B", "C", style: :dotted}]
-    output_graph(name, edges)
-  end
+    Mix.Task.run("compile")
+    Boundary.Mix.load_app()
 
-  def output_graph(name, edges) do
-    content = format_graph(name, edges)
-    write_graph(name, content)
+    app_name = Boundary.Mix.app_name()
+
+    graph =
+      format_graph(
+        "#{app_name} application",
+        app_name
+        |> Boundary.view()
+        |> Boundary.all()
+        |> Stream.filter(&(&1.app == Boundary.Mix.app_name()))
+        |> Stream.reject(&Enum.empty?(&1.deps))
+        |> Stream.filter(&Enum.empty?(&1.ancestors))
+        |> Enum.sort_by(& &1.name)
+        |> Enum.flat_map(&boundary_to_edges/1)
+      )
+
+    write_graph("app", graph) |> draw_graph
+
     :ok
   end
 
-  defp write_graph(name, content) do
-    output_path = Path.join([File.cwd!(), @output_folder])
-    dotfile_path = Path.join(output_path, "#{name}.dot")
-    pngfile_path = Path.join(output_path, "#{name}.png")
-    File.mkdir(output_path)
-    File.write!(dotfile_path, content)
-    # System.cmd("dot", ["-Tpng", dotfile_path, "-o", pngfile_path])
-    # System.cmd("open", [pngfile_path])
-    # Process.sleep(5000)
+  defp boundary_to_edges(%{name: name, deps: deps}) do
+    boundary_node = format_node(name)
+
+    deps
+    |> Enum.sort()
+    |> Enum.map(fn {name, _mode} ->
+      dep_node = format_node(name)
+      {boundary_node, dep_node}
+    end)
   end
 
   defp format_graph(title, edges) do
     """
     digraph {
-      #{edges |> Enum.map(&format_edge/1) |> Enum.join("\n  ")}
+      #{edges |> Enum.map(&format_edge/1) |> Enum.join(";\n  ")};
 
       label="#{title}";
       labelloc=top;
     }
     """
+  end
+
+  def format_node(module_name) do
+    module_name
+    |> Module.split()
+    |> Enum.join(".")
   end
 
   defp format_edge({node1, node2}), do: format_edge({node1, node2, []})
@@ -48,4 +66,21 @@ defmodule Mix.Tasks.Boundary.Visualize do
   defp format_attributes([]), do: ""
   defp format_attributes(attributes), do: " [#{attributes |> Enum.map(&format_attribute/1) |> Enum.join(", ")}]"
   defp format_attribute({name, value}), do: "#{name} = #{value}"
+
+  defp write_graph(name, content) do
+    output_path = Path.join([File.cwd!(), @output_folder])
+    dot_file_path = Path.join(output_path, "#{name}.dot")
+    File.mkdir(output_path)
+    File.write!(dot_file_path, content)
+    dot_file_path
+  end
+
+  defp draw_graph(dot_file_path) do
+    image_dir_path = Path.dirname(dot_file_path)
+    image_file_name = Path.basename(dot_file_path, ".dot")
+    image_file_path = Path.join([image_dir_path, "#{image_file_name}.png"]) |> IO.inspect()
+    System.cmd("dot", ["-Tpng", dot_file_path, "-o", image_file_path])
+    System.cmd("open", [image_file_path])
+    Process.sleep(1000)
+  end
 end
--- test/mix/tasks/visualize_test.exs
+++ test/mix/tasks/visualize_test.exs
@@ -6,21 +6,62 @@ defmodule Mix.Tasks.Boundary.VisualizeTest do
     Mix.shell(Mix.Shell.Process)
     Logger.disable(self())
 
-    TestProject.run_task("boundary.visualize")
+    TestProject.in_project(fn project ->
+      File.write!(
+        Path.join([project.path, "lib", "source.ex"]),
+        """
+        defmodule BlogEngine do
+          use Boundary
 
-    test_output_file(
-      Path.join([File.cwd!(), "dot", "Test.Graph.dot"]),
-      """
-      digraph {
-        A -> B
-        A -> C
-        B -> C [style = dotted]
+          defmodule Repo do end
+          defmodule Accounts do
+            use Boundary, deps: [Repo]
+          end
+          defmodule Articles do
+            use Boundary, deps: [Repo, Accounts]
+          end
+        end
 
-        label=\"Test.Graph\";
-        labelloc=top;
-      }
-      """
-    )
+        defmodule BlogEngineWeb do
+          use Boundary, deps: [BlogEngine], exports: []
+        end
+
+        defmodule BlogEngineApp do
+          use Boundary, deps: [BlogEngineWeb, BlogEngine], exports: []
+        end
+        """
+      )
+
+      TestProject.run_task("boundary.visualize")
+
+      test_output_file(
+        Path.join([project.path, "dot", "app.dot"]),
+        """
+        digraph {
+          BlogEngineApp -> BlogEngine;
+          BlogEngineApp -> BlogEngineWeb;
+          BlogEngineWeb -> BlogEngine;
+
+          label="test_project_2 application";
+          labelloc=top;
+        }
+        """
+      )
+
+      # test_output_file(
+      #   Path.join([project.path, "dot", "BlogEngine.dot"]),
+      #   """
+      #   digraph {
+      #     Articles -> Accounts;
+      #     Articles -> Repo;
+      #     Accounts -> Repo;
+
+      #     label="test_project_2 boundary";
+      #     labelloc=top;
+      #   }
+      #   """
+      # )
+    end)
   end
 
   def test_output_file(path, content) do
